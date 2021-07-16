2b618951bc36e6e82a553766f42fab7e01481ef9
Daniel Hazelbaker <daniel@triumph.tech>
2021-07-16 07:53:10 -0700

    - WIP: Initial work on adding generalized Structured Content parser/renderer.

---
+++ Rock/Cms/StructuredContent/StructuredContentHelper.cs
@@ -0,0 +1,207 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Rock.Cms.StructuredContent
+{
+    /// <summary>
+    /// Various helper methods when working with structured content. Automates
+    /// many of the tasks you might otherwise have to code by hand.
+    /// </summary>
+    public class StructuredContentHelper
+    {
+        #region Fields
+
+        /// <summary>
+        /// The custom block types that have been found.
+        /// </summary>
+        private static List<IStructuredContentBlockType> _customBlockTypes;
+
+        /// <summary>
+        /// The standard block types that have been found.
+        /// </summary>
+        private static List<IStructuredContentBlockType> _standardBlockTypes;
+
+        /// <summary>
+        /// The block types that will be used during standard render operations.
+        /// </summary>
+        private static Dictionary<string, IStructuredContentBlockType> _blockTypes;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the structured content JSON string.
+        /// </summary>
+        /// <value>
+        /// The structured content JSON string.
+        /// </value>
+        public string Content { get; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StructuredContentHelper"/> class.
+        /// </summary>
+        /// <param name="content">The structured content JSON string.</param>
+        public StructuredContentHelper( string content )
+        {
+            Content = content;
+        }
+
+        #endregion
+
+        #region Method
+
+        /// <summary>
+        /// Gets the custom block types registered in the system.
+        /// </summary>
+        /// <returns>An enumeration of <see cref="IStructuredContentBlockType"/> instances.</returns>
+        public virtual IEnumerable<IStructuredContentBlockType> GetCustomBlockTypes()
+        {
+            if ( _customBlockTypes == null )
+            {
+                var blockTypeTypes = Reflection.FindTypes( typeof( IStructuredContentBlockType ) )
+                    .Where( a => a.Value.Assembly != typeof( BlockType.Paragraph ).Assembly )
+                    .Select( a => a.Value );
+
+                var blockTypes = new List<IStructuredContentBlockType>();
+
+                foreach ( var type in blockTypeTypes )
+                {
+                    try
+                    {
+                        var blockType = ( IStructuredContentBlockType ) Activator.CreateInstance( type );
+                        blockTypes.Add( blockType );
+                    }
+                    catch
+                    {
+                        /* Exception intentionally ignored. */
+                    }
+                }
+
+                _customBlockTypes = blockTypes;
+            }
+
+            return _customBlockTypes;
+        }
+
+        /// <summary>
+        /// Gets the standard block types registered in the system.
+        /// </summary>
+        /// <returns>An enumeration of <see cref="IStructuredContentBlockType"/> instances.</returns>
+        public virtual IEnumerable<IStructuredContentBlockType> GetStandardBlockTypes()
+        {
+            if ( _standardBlockTypes == null )
+            {
+                var blockTypeTypes = Reflection.FindTypes( typeof( IStructuredContentBlockType ) )
+                    .Where( a => a.Value.Assembly == typeof( BlockType.Paragraph ).Assembly )
+                    .Select( a => a.Value );
+
+                var blockTypes = new List<IStructuredContentBlockType>();
+
+                foreach ( var type in blockTypeTypes )
+                {
+                    try
+                    {
+                        var blockType = ( IStructuredContentBlockType ) Activator.CreateInstance( type );
+                        blockTypes.Add( blockType );
+                    }
+                    catch
+                    {
+                        /* Exception intentionally ignored. */
+                    }
+                }
+
+                _standardBlockTypes = blockTypes;
+            }
+
+            return _standardBlockTypes;
+        }
+
+        /// <summary>
+        /// Gets the block types used for standard rendering operations.
+        /// </summary>
+        /// <returns>A dictionary of <see cref="IStructuredContentBlockType"/> instances.</returns>
+        public virtual IReadOnlyDictionary<string, IStructuredContentBlockType> GetBlockTypes()
+        {
+            if ( _blockTypes == null )
+            {
+                var blockTypes = GetStandardBlockTypes().ToDictionary( b => b.BlockType, b => b );
+
+                foreach ( var blockType in GetCustomBlockTypes() )
+                {
+                    blockTypes.AddOrIgnore( blockType.BlockType, blockType );
+                }
+            }
+
+            return _blockTypes;
+        }
+
+        /// <summary>
+        /// Renders the content and returns the string representation.
+        /// </summary>
+        /// <returns>A string that contains the rendered contents.</returns>
+        public string Render()
+        {
+            var sb = new StringBuilder();
+
+            using ( var writer = new StringWriter() )
+            {
+                Render( writer );
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Renders the block contents.
+        /// </summary>
+        /// <param name="writer">The writer to use when rendering blocks.</param>
+        public void Render( TextWriter writer )
+        {
+            Render( writer, GetBlockTypes() );
+        }
+
+        /// <summary>
+        /// Renders the block contents using the specified block types.
+        /// </summary>
+        /// <param name="writer">The writer to use when rendering blocks.</param>
+        /// <param name="blockTypes">The block types used to render</param>
+        public void Render( TextWriter writer, IReadOnlyDictionary<string, IStructuredContentBlockType> blockTypes )
+        {
+            var contentData = Content?.FromJsonOrNull<StructuredContentData>() ?? new StructuredContentData();
+
+            foreach ( var block in contentData.Blocks )
+            {
+                if ( blockTypes.TryGetValue( block.Type, out var blockType ) )
+                {
+                    blockType.Render( writer, block.Data );
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Rock/Cms/StructuredContent/StructuredContentHelperChangesExtensions.cs
@@ -0,0 +1,190 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+
+using Rock.Data;
+using Rock.Model;
+
+using StandardBlockTypes = Rock.Cms.StructuredContentHelper.StandardBlockTypes;
+
+namespace Rock.Cms.StructuredContent
+{
+    /// <summary>
+    /// Extension methods for <see cref="StructuredContentHelper"/> that provides
+    /// additional methods related to changes (and require database access).
+    /// </summary>
+    public static class StructuredContentHelperChangesExtensions
+    {
+        /// <summary>
+        /// Detects the changes that need to be applied to the database by
+        /// looking at the old content and the current content.
+        /// </summary>
+        /// <param name="helper">The content helper.</param>
+        /// <param name="oldContent">The old structured content before the save.</param>
+        /// <returns>
+        /// The changes that were detected.
+        /// </returns>
+        public static StructuredContentChanges DetectChanges( this StructuredContentHelper helper, string oldContent = "" )
+        {
+            var changes = new StructuredContentChanges();
+            var oldBinaryFileIds = new List<int>();
+            var newBinaryFileIds = new List<int>();
+            var newData = helper.Content?.FromJsonOrNull<StructuredContentData>() ?? new StructuredContentData();
+            var oldData = oldContent?.FromJsonOrNull<StructuredContentData>() ?? new StructuredContentData();
+
+            // Find all image blocks in the new data that have a binary file
+            // identifier and store it.
+            foreach ( var block in newData.Blocks )
+            {
+                if ( block.Type == StandardBlockTypes.Image )
+                {
+                    var data = ( ( JToken ) block.Data ).ToObject<StructuredContentImageData>();
+
+                    if ( data.File?.FileId != null )
+                    {
+                        newBinaryFileIds.Add( data.File.FileId.Value );
+                    }
+                }
+            }
+
+            // Find all image blocks in the old data that have a binary file
+            // identifier and store it.
+            foreach ( var block in oldData.Blocks )
+            {
+                if ( block.Type == StandardBlockTypes.Image )
+                {
+                    var data = ( ( JToken ) block.Data ).ToObject<StructuredContentImageData>();
+
+                    if ( data.File?.FileId != null )
+                    {
+                        oldBinaryFileIds.Add( data.File.FileId.Value );
+                    }
+                }
+            }
+
+            // Determine if we have any added or removed binary file identifiers.
+            changes.AddedBinaryFileIds = newBinaryFileIds.Except( oldBinaryFileIds ).ToList();
+            changes.RemovedBinaryFileIds = oldBinaryFileIds.Except( newBinaryFileIds ).ToList();
+
+            // Let all custom block types partake in the change detection.
+            foreach ( var blockType in helper.GetCustomBlockTypes() )
+            {
+                if ( !( blockType is IStructuredContentBlockTypeChanges blockTypeChanges ) )
+                {
+                    continue;
+                }
+
+                // Walk all the blocks that already existed and still exist or
+                // are new in the data.
+                foreach ( var newBlock in newData.Blocks )
+                {
+                    if ( newBlock.Type == blockTypeChanges.BlockType )
+                    {
+                        var oldBlock = oldData.Blocks.FirstOrDefault( b => b.Id == newBlock.Id );
+
+                        blockTypeChanges.DetectChanges( newBlock.Data, oldBlock?.Data, changes );
+                    }
+                }
+
+                // Walk all the old blocks that no longer exist.
+                var newBlockIds = newData.Blocks.Select( b => b.Id ).ToList();
+                foreach ( var removedBlock in oldData.Blocks.Where( b => !newBlockIds.Contains( b.Id ) ) )
+                {
+                    if ( removedBlock.Type == blockTypeChanges.BlockType )
+                    {
+                        blockTypeChanges.DetectChanges( null, removedBlock.Data, changes );
+                    }
+                }
+            }
+
+            return changes;
+        }
+
+        /// <summary>
+        /// Updates the binary files referenced by the content. This will mark newly
+        /// uploaded files as permanent and mark removed files as temporary so they
+        /// get cleaned up later. This method will call SaveChanges() so you may
+        /// want to call this inside a transaction to ensure everything either works
+        /// or is rolled back.
+        /// </summary>
+        /// <param name="helper">The content helper.</param>
+        /// <param name="changes">The changes that were returned by a previous call to DetectChanges()." />.</param>
+        /// <param name="rockContext">The rock database context to use when saving changes.</param>
+        public static void SaveDatabaseChanges( this StructuredContentHelper helper, StructuredContentChanges changes, RockContext rockContext )
+        {
+            if ( changes == null )
+            {
+                throw new ArgumentNullException( nameof( changes ) );
+            }
+
+            if ( rockContext == null )
+            {
+                throw new ArgumentNullException( nameof( rockContext ) );
+            }
+
+            bool needSave = false;
+            var binaryFileService = new BinaryFileService( rockContext );
+
+            // If there are any newly added binary files then mark them as
+            // permanent so they will persist in the database.
+            if ( changes.AddedBinaryFileIds != null && changes.AddedBinaryFileIds.Count > 0 )
+            {
+                var filesToAdd = binaryFileService.Queryable()
+                    .Where( b => changes.AddedBinaryFileIds.Contains( b.Id ) )
+                    .ToList();
+
+                filesToAdd.ForEach( b => b.IsTemporary = false );
+                needSave = true;
+            }
+
+            // If there are any removed binary files then mark them as temporary
+            // so they will be later removed by the cleanup job.
+            if ( changes.RemovedBinaryFileIds != null && changes.RemovedBinaryFileIds.Count > 0 )
+            {
+                var filesToRemove = binaryFileService.Queryable()
+                    .Where( b => changes.RemovedBinaryFileIds.Contains( b.Id ) )
+                    .ToList();
+
+                filesToRemove.ForEach( b => b.IsTemporary = true );
+                needSave = true;
+            }
+
+            // Let all custom block types partake in the change detection.
+            foreach ( var blockType in helper.GetCustomBlockTypes() )
+            {
+                if ( !( blockType is IStructuredContentBlockTypeChanges blockTypeChanges ) )
+                {
+                    continue;
+                }
+
+                if ( blockTypeChanges.ApplyDatabaseChanges( helper, changes, rockContext ) )
+                {
+                    needSave = true;
+                }
+            }
+
+            if ( needSave )
+            {
+                rockContext.SaveChanges();
+            }
+        }
+    }
+}

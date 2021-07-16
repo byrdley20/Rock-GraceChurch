749afa9d6a815f2abca23c8ac306b5f629269714
Daniel Hazelbaker <daniel@triumph.tech>
2021-07-16 14:20:36 -0700

    - Fixed bug in drag and drop tool that was firing when an image was dragged into the editor.

--- Rock.JavaScript.EditorJs/src/dragdrop.ts
+++ Rock.JavaScript.EditorJs/src/dragdrop.ts
@@ -1,9 +1,32 @@
 import EditorDragDrop from "editorjs-drag-drop";
+import EditorJS from "@editorjs/editorjs";
 
 /**
  * Custom subclass that fixes a bug with drag/drop when only one block.
  */
 export class DragDrop extends EditorDragDrop {
+    constructor(editor: EditorJS) {
+        super(editor);
+
+        this.setDragEndListener();
+    }
+
+    /**
+     * Sets the drag end events listener. This clears our drag operation
+     * so we don't fire during an external drag even (like an image being
+     * dragged into the editor).
+     */
+    protected setDragEndListener() {
+        if (!this.readOnly) {
+            const settingsButton = this.holder.querySelector('.ce-toolbar__settings-btn');
+
+            settingsButton!.setAttribute('draggable', 'true');
+            settingsButton!.addEventListener('dragend', () => {
+                this.startBlock = null;
+            });
+        }
+    }
+
     /**
      * Gets the real drop target element.
      * 
@@ -20,5 +43,17 @@ export class DragDrop extends EditorDragDrop {
         }
 
         return block;
+    }
+
+    /**
+     * Moves the dragged element to the drop position.
+     *
+     * @see {@link https://editorjs.io/blocks#move}
+     */
+    protected moveBlocks() {
+        /* Ensure this drop operation is happening for our drag operation. */
+        if (this.startBlock != null && this.endBlock != null) {
+            super.moveBlocks();
+        }
     }
 }

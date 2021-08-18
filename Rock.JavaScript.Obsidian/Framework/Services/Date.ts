d39739e21eac7ddc49d30bbcbfbbbaf56304492c
Daniel Hazelbaker <daniel@triumph.tech>
2021-08-18 10:28:23 -0700

    - Finished updates for unit testing and date formatting.

--- Rock.JavaScript.Obsidian/Framework/Services/Date.ts
+++ Rock.JavaScript.Obsidian/Framework/Services/Date.ts
@@ -17,7 +17,7 @@
 
 import RockDate, { RockDateType } from '../Util/RockDate';
 import { List } from 'linqts';
-import { padLeft } from './String';
+import { padLeft, padRight } from './String';
 
 /**
  * Transform the value into a date or null
@@ -63,7 +63,7 @@ export function toRockDateOrNull(val: unknown): RockDateType | null {
  * Transforms the value into a string like '9/13/2001'
  * @param val
  */
-export function asDateString(val: unknown) {
+export function asDateString(val: unknown): string {
     const dateOrNull = asDateOrNull(val);
 
     if (!dateOrNull) {
@@ -78,8 +78,7 @@ export function asDateString(val: unknown) {
  * Ex: March 4, 2000 => 21yrs
  * @param dateTime
  */
-export function asElapsedString ( dateTime: Date )
-{
+export function asElapsedString(dateTime: Date): string {
     const now = new Date();
     const msPerHour = 1000 * 60 * 60;
     const hoursPerDay = 24;
@@ -123,6 +122,33 @@ export function asElapsedString ( dateTime: Date )
 }
 
 
+/**
+ * Returns a blank string if the string value is 0.
+ * 
+ * @param value The value to check and return.
+ * @returns The value passed in or an empty string if it equates to zero.
+ */
+function blankIfZero(value: string): string {
+    return parseInt(value) === 0 ? '' : value;
+}
+
+/**
+ * Gets the 12 hour value of the given 24-hour number.
+ * 
+ * @param hour The hour in a 24-hour format.
+ * @returns The hour in a 12-hour format.
+ */
+function get12HourValue(hour: number): number {
+    if (hour == 0) {
+        return 12;
+    }
+    else if (hour < 13) {
+        return hour;
+    }
+    else {
+        return hour - 12;
+    }
+}
 type DateFormatterCommand = (date: Date) => string;
 
 const englishDayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -140,34 +166,186 @@ const dateFormatters: Record<string, DateFormatterCommand> = {
     'MM': date => padLeft((date.getMonth() + 1).toString(), 2, '0'),
     'M': date => (date.getMonth() + 1).toString(),
 
-
     'dddd': date => englishDayNames[date.getDay()],
     'ddd': date => englishDayNames[date.getDay()].substr(0, 3),
     'dd': date => padLeft(date.getDate().toString(), 2, '0'),
     'd': date => date.getDate().toString(),
+
+    'fffffff': date => padRight((date.getMilliseconds() * 10000).toString(), 7, '0'),
+    'ffffff': date => padRight((date.getMilliseconds() * 1000).toString(), 6, '0'),
+    'fffff': date => padRight((date.getMilliseconds() * 100).toString(), 5, '0'),
+    'ffff': date => padRight((date.getMilliseconds() * 10).toString(), 4, '0'),
+    'fff': date => padRight(date.getMilliseconds().toString(), 3, '0'),
+    'ff': date => padRight(Math.floor(date.getMilliseconds() / 10).toString(), 2, '0'),
+    'f': date => padRight(Math.floor(date.getMilliseconds() / 100).toString(), 1, '0'),
+
+    'FFFFFFF': date => blankIfZero(padRight((date.getMilliseconds() * 10000).toString(), 7, '0')),
+    'FFFFFF': date => blankIfZero(padRight((date.getMilliseconds() * 1000).toString(), 6, '0')),
+    'FFFFF': date => blankIfZero(padRight((date.getMilliseconds() * 100).toString(), 5, '0')),
+    'FFFF': date => blankIfZero(padRight((date.getMilliseconds() * 10).toString(), 4, '0')),
+    'FFF': date => blankIfZero(padRight(date.getMilliseconds().toString(), 3, '0')),
+    'FF': date => blankIfZero(padRight(Math.floor(date.getMilliseconds() / 10).toString(), 2, '0')),
+    'F': date => blankIfZero(padRight(Math.floor(date.getMilliseconds() / 100).toString(), 1, '0')),
+
+    'g': date => date.getFullYear() < 0 ? 'B.C.' : 'A.D.',
+    'gg': date => date.getFullYear() < 0 ? 'B.C.' : 'A.D.',
+
+    'hh': date => padLeft(get12HourValue(date.getHours()).toString(), 2, '0'),
+    'h': date => get12HourValue(date.getHours()).toString(),
+
+    'HH': date => padLeft(date.getHours().toString(), 2, '0'),
+    'H': date => date.getHours().toString(),
+
+    'mm': date => padLeft(date.getMinutes().toString(), 2, '0'),
+    'm': date => date.getMinutes().toString(),
+
+    'ss': date => padLeft(date.getSeconds().toString(), 2, '0'),
+    's': date => date.getSeconds().toString(),
+
+    'K': date => {
+        const offset = date.getTimezoneOffset();
+        const offsetHour = Math.abs(Math.floor(offset / 60));
+        const offsetMinute = Math.abs(offset % 60);
+        return `${offset > 0 ? '-' : '+'}${padLeft(offsetHour.toString(), 2, '0')}:${padLeft(offsetMinute.toString(), 2, '0')}`;
+    },
+
+    'tt': date => date.getHours() >= 12 ? 'PM' : 'AM',
+    't': date => date.getHours() >= 12 ? 'P' : 'A',
+
+    'zzz': date => {
+        const offset = date.getTimezoneOffset();
+        const offsetHour = Math.abs(Math.floor(offset / 60));
+        const offsetMinute = Math.abs(offset % 60);
+        return `${offset > 0 ? '-' : '+'}${padLeft(offsetHour.toString(), 2, '0')}:${padLeft(offsetMinute.toString(), 2, '0')}`;
+    },
+    'zz': date => {
+        const offset = date.getTimezoneOffset();
+        const offsetHour = Math.abs(Math.floor(offset / 60));
+        return `${offset > 0 ? '-' : '+'}${padLeft(offsetHour.toString(), 2, '0')}`;
+    },
+    'z': date => {
+        const offset = date.getTimezoneOffset();
+        const offsetHour = Math.abs(Math.floor(offset / 60));
+        return `${offset > 0 ? '-' : '+'}${offsetHour}`;
+    },
+
+    ':': () => ':',
+    '/': () => '/'
 };
+
 const dateFormatterKeys = new List<string>(Object.keys(dateFormatters))
     .OrderByDescending(k => k.length)
     .ToArray();
 
-export function formatAspDate(date: Date, format: string): string {
+const standardDateFormats: Record<string, DateFormatterCommand> = {
+    'd': date => formatAspDate(date, 'M/dd/yyyy'),
+    'D': date => formatAspDate(date, 'dddd, MMMM dd, yyyy'),
+    't': date => formatAspDate(date, 'h:mm tt'),
+    'T': date => formatAspDate(date, 'h:mm:ss tt'),
+    'M': date => formatAspDate(date, 'MMMM dd'),
+    'm': date => formatAspDate(date, 'MMMM dd'),
+    'Y': date => formatAspDate(date, 'yyyy MMMM'),
+    'y': date => formatAspDate(date, 'yyyy MMMM'),
+    'f': date => `${formatAspDate(date, 'D')} ${formatAspDate(date, 't')}`,
+    'F': date => `${formatAspDate(date, 'D')} ${formatAspDate(date, 'T')}`,
+    'g': date => `${formatAspDate(date, 'd')} ${formatAspDate(date, 't')}`,
+    'G': date => `${formatAspDate(date, 'd')} ${formatAspDate(date, 'T')}`,
+    'o': date => formatAspDate(date, `yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz`),
+    'O': date => formatAspDate(date, `yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz`),
+    'r': date => formatAspDate(date, `ddd, dd MMM yyyy HH':'mm':'ss 'GMT'`),
+    'R': date => formatAspDate(date, `ddd, dd MMM yyyy HH':'mm':'ss 'GMT'`),
+    's': date => formatAspDate(date, `yyyy'-'MM'-'dd'T'HH':'mm':'ss`),
+    'u': date => formatAspDate(date, `yyyy'-'MM'-'dd HH':'mm':'ss'Z'`),
+    'U': date => {
+        const utcDate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds());
+        return formatAspDate(utcDate, `F`);
+    },
+};
+
+/**
+ * Formats the Date object using custom format specifiers.
+ * 
+ * @param date The date object to be formatted.
+ * @param format The custom format string.
+ * @returns A string that represents the date in the specified format.
+ */
+function formatAspCustomDate(date: Date, format: string): string {
     let result = '';
 
-    for (let i = 0; i < format.length; i++) {
+    for (let i = 0; i < format.length;) {
         let matchFound = false;
 
         for (const k of dateFormatterKeys) {
             if (format.substr(i, k.length) === k) {
                 result += dateFormatters[k](date);
                 matchFound = true;
+                i += k.length;
                 break;
             }
         }
 
-        if (!matchFound) {
-            result += format[i];
+        if (matchFound) {
+            continue;
+        }
+
+        if (format[i] === '\\') {
+            i++;
+            if (i < format.length) {
+                result += format[i++];
+            }
+        }
+        else if (format[i] === '\'') {
+            i++;
+            for (; i < format.length && format[i] !== '\''; i++) {
+                result += format[i];
+            }
+            i++;
+        }
+        else if (format[i] === '"') {
+            i++;
+            for (; i < format.length && format[i] !== '"'; i++) {
+                result += format[i];
+            }
+            i++;
+        }
+        else {
+            result += format[i++];
         }
     }
 
     return result;
+}
+
+/**
+ * Formats the Date object using a standard format string.
+ * 
+ * @param date The date object to be formatted.
+ * @param format The standard format specifier.
+ * @returns A string that represents the date in the specified format.
+ */
+function formatAspStandardDate(date: Date, format: string): string {
+    if (standardDateFormats[format] !== undefined) {
+        return standardDateFormats[format](date);
+    }
+
+    return format;
+}
+
+/**
+ * Formats the given Date object using nearly the same rules as the ASP C#
+ * format methods.
+ * 
+ * @param date The date object to be formatted.
+ * @param format The format string to use.
+ */
+export function formatAspDate(date: Date, format: string): string {
+    if (format.length === 1) {
+        return formatAspStandardDate(date, format);
+    }
+    else if (format.length === 2 && format[0] === '%') {
+        return formatAspCustomDate(date, format[1]);
+    }
+    else {
+        return formatAspCustomDate(date, format);
+    }
 }
--- RockWeb/Obsidian/Services/Date.js
+++ RockWeb/Obsidian/Services/Date.js
@@ -1,6 +1,6 @@
 System.register(["../Util/RockDate", "linqts", "./String"], function (exports_1, context_1) {
     "use strict";
-    var RockDate_1, linqts_1, String_1, englishDayNames, englishMonthNames, dateFormatters, dateFormatterKeys;
+    var RockDate_1, linqts_1, String_1, englishDayNames, englishMonthNames, dateFormatters, dateFormatterKeys, standardDateFormats;
     var __moduleName = context_1 && context_1.id;
     function asDateOrNull(val) {
         if (val === undefined || val === null) {
@@ -64,22 +64,77 @@ System.register(["../Util/RockDate", "linqts", "./String"], function (exports_1,
         return `${Math.round(totalYears)}yrs`;
     }
     exports_1("asElapsedString", asElapsedString);
-    function formatAspDate(date, format) {
+    function blankIfZero(value) {
+        return parseInt(value) === 0 ? '' : value;
+    }
+    function get12HourValue(hour) {
+        if (hour == 0) {
+            return 12;
+        }
+        else if (hour < 13) {
+            return hour;
+        }
+        else {
+            return hour - 12;
+        }
+    }
+    function formatAspCustomDate(date, format) {
         let result = '';
-        for (let i = 0; i < format.length; i++) {
+        for (let i = 0; i < format.length;) {
             let matchFound = false;
             for (const k of dateFormatterKeys) {
                 if (format.substr(i, k.length) === k) {
                     result += dateFormatters[k](date);
                     matchFound = true;
+                    i += k.length;
                     break;
                 }
             }
-            if (!matchFound) {
-                result += format[i];
+            if (matchFound) {
+                continue;
+            }
+            if (format[i] === '\\') {
+                i++;
+                if (i < format.length) {
+                    result += format[i++];
+                }
+            }
+            else if (format[i] === '\'') {
+                i++;
+                for (; i < format.length && format[i] !== '\''; i++) {
+                    result += format[i];
+                }
+                i++;
+            }
+            else if (format[i] === '"') {
+                i++;
+                for (; i < format.length && format[i] !== '"'; i++) {
+                    result += format[i];
+                }
+                i++;
+            }
+            else {
+                result += format[i++];
             }
         }
         return result;
+    }
+    function formatAspStandardDate(date, format) {
+        if (standardDateFormats[format] !== undefined) {
+            return standardDateFormats[format](date);
+        }
+        return format;
+    }
+    function formatAspDate(date, format) {
+        if (format.length === 1) {
+            return formatAspStandardDate(date, format);
+        }
+        else if (format.length === 2 && format[0] === '%') {
+            return formatAspCustomDate(date, format[1]);
+        }
+        else {
+            return formatAspCustomDate(date, format);
+        }
     }
     exports_1("formatAspDate", formatAspDate);
     return {
@@ -111,10 +166,84 @@ System.register(["../Util/RockDate", "linqts", "./String"], function (exports_1,
                 'ddd': date => englishDayNames[date.getDay()].substr(0, 3),
                 'dd': date => String_1.padLeft(date.getDate().toString(), 2, '0'),
                 'd': date => date.getDate().toString(),
+                'fffffff': date => String_1.padRight((date.getMilliseconds() * 10000).toString(), 7, '0'),
+                'ffffff': date => String_1.padRight((date.getMilliseconds() * 1000).toString(), 6, '0'),
+                'fffff': date => String_1.padRight((date.getMilliseconds() * 100).toString(), 5, '0'),
+                'ffff': date => String_1.padRight((date.getMilliseconds() * 10).toString(), 4, '0'),
+                'fff': date => String_1.padRight(date.getMilliseconds().toString(), 3, '0'),
+                'ff': date => String_1.padRight(Math.floor(date.getMilliseconds() / 10).toString(), 2, '0'),
+                'f': date => String_1.padRight(Math.floor(date.getMilliseconds() / 100).toString(), 1, '0'),
+                'FFFFFFF': date => blankIfZero(String_1.padRight((date.getMilliseconds() * 10000).toString(), 7, '0')),
+                'FFFFFF': date => blankIfZero(String_1.padRight((date.getMilliseconds() * 1000).toString(), 6, '0')),
+                'FFFFF': date => blankIfZero(String_1.padRight((date.getMilliseconds() * 100).toString(), 5, '0')),
+                'FFFF': date => blankIfZero(String_1.padRight((date.getMilliseconds() * 10).toString(), 4, '0')),
+                'FFF': date => blankIfZero(String_1.padRight(date.getMilliseconds().toString(), 3, '0')),
+                'FF': date => blankIfZero(String_1.padRight(Math.floor(date.getMilliseconds() / 10).toString(), 2, '0')),
+                'F': date => blankIfZero(String_1.padRight(Math.floor(date.getMilliseconds() / 100).toString(), 1, '0')),
+                'g': date => date.getFullYear() < 0 ? 'B.C.' : 'A.D.',
+                'gg': date => date.getFullYear() < 0 ? 'B.C.' : 'A.D.',
+                'hh': date => String_1.padLeft(get12HourValue(date.getHours()).toString(), 2, '0'),
+                'h': date => get12HourValue(date.getHours()).toString(),
+                'HH': date => String_1.padLeft(date.getHours().toString(), 2, '0'),
+                'H': date => date.getHours().toString(),
+                'mm': date => String_1.padLeft(date.getMinutes().toString(), 2, '0'),
+                'm': date => date.getMinutes().toString(),
+                'ss': date => String_1.padLeft(date.getSeconds().toString(), 2, '0'),
+                's': date => date.getSeconds().toString(),
+                'K': date => {
+                    const offset = date.getTimezoneOffset();
+                    const offsetHour = Math.abs(Math.floor(offset / 60));
+                    const offsetMinute = Math.abs(offset % 60);
+                    return `${offset > 0 ? '-' : '+'}${String_1.padLeft(offsetHour.toString(), 2, '0')}:${String_1.padLeft(offsetMinute.toString(), 2, '0')}`;
+                },
+                'tt': date => date.getHours() >= 12 ? 'PM' : 'AM',
+                't': date => date.getHours() >= 12 ? 'P' : 'A',
+                'zzz': date => {
+                    const offset = date.getTimezoneOffset();
+                    const offsetHour = Math.abs(Math.floor(offset / 60));
+                    const offsetMinute = Math.abs(offset % 60);
+                    return `${offset > 0 ? '-' : '+'}${String_1.padLeft(offsetHour.toString(), 2, '0')}:${String_1.padLeft(offsetMinute.toString(), 2, '0')}`;
+                },
+                'zz': date => {
+                    const offset = date.getTimezoneOffset();
+                    const offsetHour = Math.abs(Math.floor(offset / 60));
+                    return `${offset > 0 ? '-' : '+'}${String_1.padLeft(offsetHour.toString(), 2, '0')}`;
+                },
+                'z': date => {
+                    const offset = date.getTimezoneOffset();
+                    const offsetHour = Math.abs(Math.floor(offset / 60));
+                    return `${offset > 0 ? '-' : '+'}${offsetHour}`;
+                },
+                ':': () => ':',
+                '/': () => '/'
             };
             dateFormatterKeys = new linqts_1.List(Object.keys(dateFormatters))
                 .OrderByDescending(k => k.length)
                 .ToArray();
+            standardDateFormats = {
+                'd': date => formatAspDate(date, 'M/dd/yyyy'),
+                'D': date => formatAspDate(date, 'dddd, MMMM dd, yyyy'),
+                't': date => formatAspDate(date, 'h:mm tt'),
+                'T': date => formatAspDate(date, 'h:mm:ss tt'),
+                'M': date => formatAspDate(date, 'MMMM dd'),
+                'm': date => formatAspDate(date, 'MMMM dd'),
+                'Y': date => formatAspDate(date, 'yyyy MMMM'),
+                'y': date => formatAspDate(date, 'yyyy MMMM'),
+                'f': date => `${formatAspDate(date, 'D')} ${formatAspDate(date, 't')}`,
+                'F': date => `${formatAspDate(date, 'D')} ${formatAspDate(date, 'T')}`,
+                'g': date => `${formatAspDate(date, 'd')} ${formatAspDate(date, 't')}`,
+                'G': date => `${formatAspDate(date, 'd')} ${formatAspDate(date, 'T')}`,
+                'o': date => formatAspDate(date, `yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz`),
+                'O': date => formatAspDate(date, `yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz`),
+                'r': date => formatAspDate(date, `ddd, dd MMM yyyy HH':'mm':'ss 'GMT'`),
+                'R': date => formatAspDate(date, `ddd, dd MMM yyyy HH':'mm':'ss 'GMT'`),
+                's': date => formatAspDate(date, `yyyy'-'MM'-'dd'T'HH':'mm':'ss`),
+                'u': date => formatAspDate(date, `yyyy'-'MM'-'dd HH':'mm':'ss'Z'`),
+                'U': date => {
+                    const utcDate = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds());
+                    return formatAspDate(utcDate, `F`);
+                },
+            };
         }
     };
 });
